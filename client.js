bb384b7d0519e0db4aae9f81381aa4fad19f3568
Daniel Lever <daniel@ed.com.au>
2017-02-09 11:12:19 +1100

    Upgraded client to React Router 4

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const React = require('react');
-const ReactRouter = require('react-router');
+const ReactRouter = require('react-router-dom');
 const merely = require('merely');
 
 merely.plugin('merely-react-router', (options, isDev) => {
